d44dd1f4b044f22bcb55d44e0a6e55ce58a11ed1
devriazul <riazul56@gmail.com>
2021-09-24 16:41:15 +0600

    video 4 completed

---
+++ 004/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Operator</title>
+</head>
+
+<body>
+    Power: <?php echo pow(2,5); ?>
+    <br>
+
+    sqrt: <?php echo sqrt(4); ?>
+    <br>
+
+    sin: <?php echo sin(90); ?>
+    <br>
+
+    random: <?php echo rand(); ?>
+    <br>
+
+    random fixing: <?php echo rand(1,25); ?>
+    <br>
+
+    divide: <?php echo 10%5; ?>
+    <br>
+
+    increment: <?php $x=10;  $x++; echo $x ?>
+    <br>
+
+    increment: <?php $x=10;  $x--; echo $x ?>
+    <br>
+</body>
+
+</html>
