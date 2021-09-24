47213ce1b4ee6d3c453a36585edf0cfe2df09538
devriazul <riazul56@gmail.com>
2021-09-24 21:05:37 +0600

    video 12 completed

---
+++ 012/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    // $x=0;
+
+    // do{
+    //     echo $x;
+    //     $x++;
+        
+    // }while($x<=10);
+
+
+    // for($x=0; $x<=5; $x++){
+    //     echo $x;
+    // }
+
+        $colors = array("red","green","blue");
+
+        foreach($colors as $color){
+            echo $color;
+        }
+?>
