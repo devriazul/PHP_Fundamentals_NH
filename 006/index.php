beb463f49509cf00771b45dc34d1a47746e53c3b
devriazul <riazul56@gmail.com>
2021-09-24 19:18:32 +0600

    video 6 completed

---
+++ 006/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arithmetic operator</title>
+</head>
+
+<body>
+    <?php
+        $x=5;
+        $y=3;
+        $sum=$x+$y;
+        echo $sum;
+
+    echo '<br>';
+    
+        $minus= $x-$y;
+        echo $minus;
+        echo '<br>';
+
+        $multiply= $x*$y;
+        echo $multiply;
+        echo '<br>';
+
+        $divide= $x/$y;
+        echo $divide;
+        echo '<br>';
+
+        $percent= $x%$y;
+        echo $percent;
+        echo '<br>';
+
+    ?>
+</body>
+
+</html>
