d32a04d73f3730eafd88b33f15a5427faed868bf
devriazul <riazul56@gmail.com>
2021-09-24 20:46:23 +0600

    video 9 completed

---
+++ 010/index.php
@@ -0,0 +1,35 @@
+<?php
+
+    $x=10;
+    $y=20;
+
+    // Theory 01
+
+    // if($x=10 and $y=20){
+    //     echo "They are equal";
+    // }else{
+    //     echo "They are not equal";
+    // }
+
+    //alternate writing system of Theory 01
+    
+    // if($x=10 & $y=20){
+    //     echo "They are equal";
+    // }else{
+    //     echo "They are not equal";
+    // }
+    
+    // Theory 02
+    // if($x=10 or $y=20){
+    //     echo "They are equal";
+    // }else{
+    //     echo "They are not equal";
+    // }
+
+    if($x=10 xor $y=20){
+        echo "They are equal";
+    }else{
+        echo "They are not equal";
+    }
+
+?>
